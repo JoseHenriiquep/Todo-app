065410eb5d22d07072fe66f787a3a2a9fe56f317
JoseHenriiquep <josehenriquenetinho@gmail.com>
2025-04-13 01:03:02 -0300

    task-detail

--- todo-app-frontend/src/app/components/tasks/task-detail/task-detail.component.ts
+++ todo-app-frontend/src/app/components/tasks/task-detail/task-detail.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { ActivatedRoute } from '@angular/router';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
   selector: 'app-task-detail',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    CommonModule
+  ],
   templateUrl: './task-detail.component.html',
   styleUrl: './task-detail.component.css'
 })
-export class TaskDetailComponent {
+export class TaskDetailComponent implements OnInit{
 
+  taskId!: string;
+  task: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private taskService: TaskService
+  ){}
+  
+  ngOnInit(): void{
+    this.taskId = this.activatedRoute.snapshot.paramMap.get('id')!;  
+    this.taskService.getTaskById(this.taskId).subscribe(task => {
+      this.task = task
+    })
+  }
 }
